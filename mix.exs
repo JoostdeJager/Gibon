9d5762d4938357b5b9cb3a274042907e234cb38f
Joost de Jager <joostdejager2002@outlook.com>
2020-12-22 14:31:41 +0100

    added ibrowse

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule Gibon.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:circuits_uart, "~> 1.4"}
+      {:circuits_uart, "~> 1.4"},
+      {:ibrowse, "~> 4.4"}
     ]
   end
 
